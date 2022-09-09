8c0790e4af4b86b5efef45f6bd46543cd2d4d674
Vitalii Kalinchyk <karlkory@gmail.com>
2022-09-09 14:00:04 +0300

    done

--- src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
+++ src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
@@ -2,34 +2,53 @@ package com.epam.autotasks.collections;
 
 import java.util.AbstractSet;
 import java.util.Iterator;
+import java.util.TreeSet;
 
 class RangedOpsIntegerSet extends AbstractSet<Integer> {
 
+    private final TreeSet<Integer> elements = new TreeSet<>();
+
     public boolean add(int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+
+        boolean result = false;
+
+        for (int i = fromInclusive; i < toExclusive; i++) {
+            if (add(i)) {
+                result = true;
+            }
+        }
+
+        return result;
     }
 
     public boolean remove(int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+        boolean result = false;
+
+        for (int i = fromInclusive; i < toExclusive; i++) {
+            if (remove(i))
+                result = true;
+        }
+
+        return result;
     }
 
     @Override
     public boolean add(final Integer integer) {
-        throw new UnsupportedOperationException();
+        return elements.add(integer);
     }
 
     @Override
     public boolean remove(final Object o) {
-        throw new UnsupportedOperationException();
+        return elements.remove(o);
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        throw new UnsupportedOperationException();
+        return elements.iterator();
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return elements.size();
     }
 }
