a2ff15c36d8563fdead0182b827f1efe1e46de08
Vitalii Kalinchyk <karlkory@gmail.com>
2022-09-14 15:14:15 +0300

    better than oracle

--- src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
+++ src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
@@ -1,99 +1,135 @@
 package com.epam.autotasks.collections;
 
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 class RangedOpsIntegerSet extends AbstractSet<Integer> {
 
     private Integer[] elements = new Integer[0];
 
-    public boolean add(int fromInclusive, int toExclusive) {
+    private int size;
 
-        boolean result = false;
+    private static class SearchResult {
+        int index;
 
-        for (int i = fromInclusive; i < toExclusive; i++) {
-            if (add(i)) {
-                result = true;
-            }
+        boolean contains;
+
+        public SearchResult(int index, boolean contains) {
+            this.index = index;
+            this.contains = contains;
         }
-
-        return result;
-    }
-
-    public boolean remove(int fromInclusive, int toExclusive) {
-        boolean result = false;
-
-        for (int i = fromInclusive; i < toExclusive; i++) {
-            if (remove(i))
-                result = true;
-        }
-
-        return result;
     }
 
     @Override
     public boolean add(final Integer integer) {
-        if (elements.length == 0) {
-            elements = new Integer[]{integer};
+
+        if (size == 0) {
+            elements = new Integer[10];
+            elements[0] = integer;
+            size++;
             return true;
         }
-        int index = elements.length;
-        for (int i = 0; i < elements.length; i++) {
-            if (elements[i].equals(integer)) {
-                return false;
-            }
-            if (elements[i] > integer) {
-                index = i;
-                break;
-            }
+
+        SearchResult searchResult = binarySearch(integer);
+        if (searchResult.contains) {
+            return false;
         }
-        Integer[] array = new Integer[elements.length + 1];
-        for (int i = 0; i < array.length; i++) {
-            if (i < index) {
-                array[i] = elements[i];
-            } else if (i == index) {
-                array[i] = integer;
-            } else {
-                array[i] = elements[i - 1];
-            }
+        elementsCopy(searchResult.index, integer);
+        size++;
+        return true;
+    }
+
+    private void elementsCopy(int index, int integer) {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, size * 2);
         }
-        elements = array;
+
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = integer;
+    }
+
+    public boolean add(int fromInclusive, int toExclusive) {
+
+        int indexFirst = binarySearch(fromInclusive).index;
+        int indexLast = binarySearch(toExclusive, indexFirst).index;
+
+        int range = toExclusive - fromInclusive;
+        int newSize = size - indexLast + indexFirst + range;
+        if (newSize == size)
+            return false;
+
+        elements = Arrays.copyOf(elements, newSize + 10);
+        System.arraycopy(elements,indexLast, elements, indexFirst + range, size - indexLast);
+        for (int i = fromInclusive; i < toExclusive; i++) {
+            elements[indexFirst++] = i;
+        }
+
+        size = elements.length - 10;
         return true;
     }
 
     @Override
     public boolean remove(final Object o) {
-        for (int i = 0; i < size(); i++) {
-            if (elements[i].equals(o)) {
-                Integer[] array = new Integer[elements.length - 1];
-                for (int j = 0; j < elements.length; j++) {
-                    if (j < i) {
-                        array[j] = elements[j];
-                    } else if (j > i) {
-                        array[j - 1] = elements[j];
-                    }
-                }
-                elements = array;
-                return true;
-            }
+        SearchResult searchResult = binarySearch((Integer) o);
+        if (searchResult.contains) {
+            int index = searchResult.index;
+            System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+            size--;
+            return true;
         }
         return false;
     }
 
+    public boolean remove(int fromInclusive, int toExclusive) {
+
+        int indexFirst = binarySearch(fromInclusive).index;
+        int indexLast = binarySearch(toExclusive, indexFirst).index;
+
+        int countsToRemove = indexLast - indexFirst;
+        if (countsToRemove == 0)
+            return false;
+
+        System.arraycopy(elements,indexLast, elements, indexFirst, size - indexLast);
+        size -= countsToRemove;
+        return true;
+    }
+
+    private SearchResult binarySearch(int key) {
+        return binarySearch(key, 0);
+    }
+
+    private SearchResult binarySearch(int key, int startIndex) {
+        int low = startIndex;
+        int high = size - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int cmp = elements[mid].compareTo(key);
+
+            if (cmp < 0)
+                low = mid + 1;
+            else if (cmp > 0)
+                high = mid - 1;
+            else
+                return new SearchResult(mid, true);
+        }
+        return new SearchResult(low, false);
+    }
+
+
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator<>() {
-            int cursor = 0;
 
+        return new Iterator<>() {
+
+            int cursor = 0;
             @Override
             public boolean hasNext() {
-                return cursor != elements.length;
+                return cursor != size;
             }
 
             @Override
             public Integer next() {
-                if (cursor == elements.length)
+                if (cursor == size)
                     throw new NoSuchElementException();
                 return elements[cursor++];
             }
@@ -102,6 +138,6 @@ class RangedOpsIntegerSet extends AbstractSet<Integer> {
 
     @Override
     public int size() {
-        return elements.length;
+        return size;
     }
 }
